3a9458d955ea489fa493c6a1db300dbc15e1558b
George Velentzas <geovelentzas@gmail.com>
2018-01-02 10:58:33 +0200

    Change comments to English

--- lab1/meros2.m
+++ lab1/meros2.m
@@ -1,61 +1,53 @@
-%************ ΚΑΘΑΡΙΣΜΑ ΟΛΩΝ ΚΑΙ ΚΛΕΙΣΙΜΟ ΠΑΡΑΘΥΡΩΝ ***********************
+%************ clear all and get screen size ******* ***********************
 clear all; close all; clc;
 scrsz = get(0,'ScreenSize');
 %**************************************************************************
 
-
-
-%************ ΚΛΗΣΗ ΤΗΣ read_all_speakers ΓΙΑ ΔΗΜΙΟΥΡΓΙΑ ******************
-%********************** ΠΙΝΑΚΩΝ ΜΕ ΤΟΥΣ MFCCs *****************************
-C4 = read_all_speakers(4);          %βλεπε συναρτηση read_all_speakers
+%***** call read_all_speakers for making matrices with mfccs **************
+C4 = read_all_speakers(4);          %see read_all_speakers
 C6 = read_all_speakers(6);
 %**************************************************************************
 
 
-
-%********* ΚΛΗΣΗ ΤΗΣ MFCC ΣΥΝΑΡΤΗΣΗΣ ΓΙΑ ΔΗΜΙΟΥΡΓΙΑ ΠΙΝΑΚΑ ****************
-%************ΜΕ ΤΟΥΣ MFCCs ΓΙΑ ΜΙΑ ΕΚΦΩΝΗΣΗ ΚΑΘΕ ΨΗΦΙΟΥ *******************
-%**************ΚΑΙ ΕΜΦΑΝΙΣΗ ΤΩΝ ΣΥΝΤΕΛΕΣΤΩΝ ΜΕ BAR3 ***********************
-C4_1 = MFCC('four1.wav');           %βλεπε συναρτηση MFCC
+%************** visualize some coefficients *******************************
+C4_1 = MFCC('four1.wav');           %see MFCC
 C6_1 = MFCC('six1.wav');
 figure('Position',[100 100 scrsz(3)-200 scrsz(4)-200]);
 subplot(1,2,1); 
 bar3(C4_1(:,:)); 
 axis square;
-title('MFCC του 1ου ομιλητη για εκφώνηση του αριθμου "4" ')
-ylabel('χρονικό παράθυρο');
-xlabel('Συντελεστης MFCC')
+title('MFCC of 1st speaker for pronouncing digit 4')
+ylabel('time window');
+xlabel('MFCC')
 subplot(1,2,2); 
 bar3(C6_1(:,:)); 
 axis square;
-title('MFCC του 1ου ομιλητη για εκφώνηση του αριθμου "6" ')
+title('MFCC of 1st speaker for pronouncing digit 6')
 %**************************************************************************
 
 
-
-%************ ΕΜΦΑΝΙΣΗ ΤΩΝ ΙΣΤΟΓΡΑΜΜΑΤΩΝ ΓΙΑ ΤΟΥΣ ΖΗΤΟΥΜΕΝΟΥΣ *************
-%*********** ΣΥΝΤΕΛΕΣΤΕΣ ΓΙΑ ΟΛΕΣ ΤΙΣ ΕΚΦΩΝΗΣΕΙΣ ΚΑΘΕ ΨΗΦΙΟΥ***************
+%*** visualization of histograms for some coefficients ********************
 figure;
 subplot(2,1,1);
-histfit(C4(:,3),25);                   %ιστογραμμα του δευτερου συντελεστη
-title('C_i (2) για το ψηφιο "4"');
-ylabel('συχνοτητα εμφανισης')
+histfit(C4(:,3),25);                   %histogram of 2nd coefficient
+title('C_i (2) for digit 4');
+ylabel('frequency of appearance')
 subplot(2,1,2);
-histfit(C4(:,13),25);                  %ιστογραμμα του δευτερου συντελεστη
-title('C_i (12) για το ψηφιο "4"');
-xlabel('διαστηματα τιμων')
-ylabel('συχνοτητα εμφανισης')
+histfit(C4(:,13),25);                  %histogram of 12th coefficient
+title('C_i (12) for digit 4');
+xlabel('values')
+ylabel('frequency of appearance')
 
 figure;
 subplot(2,1,1);
-histfit(C6(:,3),25);                   %ιστογραμμα του δευτερου συντελεστη
-title('C_i (2) για το ψηφιο "6"');
-ylabel('συχνοτητα εμφανισης')
+histfit(C6(:,3),25);                   %histogram of 2nd coefficient
+title('C_i (2) for digit 6');
+ylabel('frequency of appearance')
 subplot(2,1,2);
-histfit(C6(:,13),25);                  %ιστογραμμα του δευτερου συντελεστη
-title('C_i (12) για το ψηφιο "6"');
-xlabel('διαστηματα τιμων')
-ylabel('συχνοτητα εμφανισης')
+histfit(C6(:,13),25);                  %histogram of 12th coefficient
+title('C_i (12) for digit 6');
+xlabel('values')
+ylabel('frequency of appearance')
 %**************************************************************************
 
 
